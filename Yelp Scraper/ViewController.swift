9d915a1ea9687fb581d300d191c725b9e1ff9f29
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-03-05 16:53:13 -0800

    Now scrapes ammenities

--- Yelp Scraper/ViewController.swift
+++ Yelp Scraper/ViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 import WebKit
 import SwiftSoup
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.webView = WKWebView()
+        let config = WKWebViewConfiguration()
+        let pref = WKWebpagePreferences()
+        pref.preferredContentMode = .desktop
+        config.defaultWebpagePreferences = pref
+        self.webView = WKWebView(frame: .zero, configuration: config)
+        self.webView.navigationDelegate = self
         view = webView
         
         let url = URL(string: "https://www.yelp.com/biz/kedai-makan-seattle-4")!
@@ -24,18 +29,35 @@ class ViewController: UIViewController {
         webView.load(request)
     }
     
-    func getAmmenities() {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Task {
+            await getAmmenities(webView: webView)
+        }
+    }
+    
+    func getAmmenities(webView: WKWebView) async {
         do {
-            let html: String = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>"
+            
+            let result = try await webView.evaluateJavaScript("document.body.innerHTML")
+            guard let html = result as? String else {
+                print("Failed to get HTML")
+                return
+            }
                 let doc: Document = try SwiftSoup.parse(html)
-                let link: Element = try doc.select("a").first()!
-                
-                let text: String = try doc.body()!.text() // "An example link."
-                let linkHref: String = try link.attr("href") // "http://example.com/"
-                let linkText: String = try link.text() // "example"
-                
-                let linkOuterH: String = try link.outerHtml() // "<a href="http://example.com/"><b>example</b></a>"
-                let linkInnerH: String = try link.html() // "<b>example</b>"
+                let sections: Elements = try doc.select("section")
+            for section in sections {
+                let sectionLabel: String = try section.attr("aria-label")
+                if sectionLabel.contains("Amenities and More") {
+                    print("Found It!")
+                    let ammenitiesSection = section
+                    let divs: Elements = try ammenitiesSection.select("div")
+                    let divArray = Array(Set(try divs.eachText()))
+                    let ammenities = divArray.filter { str in
+                        str.count < 50 && str != "Amenities and More"
+                    }
+                    break
+                }
+            }
         } catch {
             
         }
